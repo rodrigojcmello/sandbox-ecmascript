405f32c98c5a2cbf1d57a7c3c57a3828c5b236f3
Rodrigo Mello <rodrigojcmello@gmail.com>
2022-09-12 01:17:49 -0300

    feat: ...

--- algorithms/recursion/recursion.test.ts
+++ algorithms/recursion/recursion.test.ts
@@ -26,6 +26,10 @@ describe('Recursion', () => {
   });
 
   it('String to array ((1), (((2), 3), ((4, (5)), 6), (7, 8)), 9)', () => {
+    expect(stringToArray('((1, 2), 3, 4)')).toEqual([[1, 2], 3, 4]);
+  });
+
+  it('String to array ((1), (((2), 3), ((4, (5)), 6), (7, 8)), 9)', () => {
     expect(stringToArray('((1, 2, 3, 4, 5, 6, 7, (8, 9)))')).toEqual([
       [1, 2, 3, 4, 5, 6, 7, [8, 9]],
     ]);
